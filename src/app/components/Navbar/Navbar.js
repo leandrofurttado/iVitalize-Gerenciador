4e721dc0ebd260d6e8bb36d2aa43b8f9e5db07e1
Gabriel Fernandes <gabrielgnsr2000@hotmail.com>
2023-11-18 13:10:42 -0300

    Function Logout e Estilizacoes

--- src/app/components/Navbar/Navbar.js
+++ src/app/components/Navbar/Navbar.js
@@ -1,7 +1,21 @@
+'use client'
+
+//imports auth
+import { NextAuthOptions } from '@/app/api/auth/[...nextauth]/route'
+import { getServerSession } from 'next-auth'
+import { signOut } from 'next-auth/react'
+
 import styles from './Navbar.module.css'
 import { Poppins } from 'next/font/google'
 import Image from 'next/image'
 import Link from 'next/link'
+
+import { MagicMotion } from "react-magic-motion"
+
+
+
+import { useState } from 'react'
+import { useRouter } from 'next/navigation'
 
 
 const poppins = Poppins({
@@ -9,34 +23,69 @@ const poppins = Poppins({
     weight: '500'
 })
 
-export default function Navbar() {
+
+
+export default  function Navbar() {
+
+    const router = useRouter()
+    //Desloga
+    async function logout(){
+        await signOut({
+            redirect: false
+        })
+        //quando deslogar volta para a raiz
+        router.replace('/')
+    }
+
+    const [clickImg, setClickImg] = useState(false)
+
+    function handleClick() {
+        //Quando mouse estiver por cima, ativa o modal
+        setClickImg(!clickImg)
+    }
+
+
     return (
+
         <nav className={`${poppins.className} ${styles.NavBar}`}>
             <div className={styles.aboveBar}>
                 <Image
-                    src={`/images/Slogan.png`}
-                    width={200}
-                    height={80}
+                    src={`/images/ivitalize_ngtv.png`}
+                    width={290}
+                    height={50}
                     alt='Logo'
                 />
-                <div className={styles.imageUser}>
+                <div className={styles.imageUser} onClick={handleClick}>
                     <Image
                         src=''
                         width={20}
                         height={20}
                         alt='UserImg'
                     />
+
                 </div>
+
+                {clickImg && (
+                    <MagicMotion>
+                        <div className={`${styles.modalUser} ${styles.show}`}>
+                            <button>Perfil</button>
+                            <button onClick={logout}>Sair</button>
+                        </div>
+                    </MagicMotion>
+
+                )}
+
+
             </div>
             <ul className={styles.RedirectBar}>
                 <li>
-                    <Link href='/alunos'> Alunos</Link>
+                    <Link href='/alunos'> ALUNOS</Link>
                 </li>
                 <li>
-                    <Link href='/colaboradores'> Colaboradores</Link>
+                    <Link href='/colaboradores'> COLABORADORES</Link>
                 </li>
                 <li>
-                    <Link href='/agenda'> Agenda</Link>
+                    <Link href='/agenda'> AGENDA</Link>
                 </li>
             </ul>
         </nav>
