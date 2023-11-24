9a81785a9c6f4e322e6fc68833a3236d5aa6e2b5
Gabriel Fernandes <gabrielgnsr2000@hotmail.com>
2023-11-24 15:40:50 -0300

    Atualizacoes Page Cadastro

--- src/app/(alunos-auth)/alunos/(cadastro-auth)/cadastro/page.js
+++ src/app/(alunos-auth)/alunos/(cadastro-auth)/cadastro/page.js
@@ -13,7 +13,7 @@ export default function Page() {
     return (
         <main className={`${poppins.className} ${styles.Main}`}>
             <div className={styles.barAlunos}>
-                <h1>ALUNOS</h1>
+                <h1>CADASTRO</h1>
             </div>
             <div className={styles.containerInfos}>
                 <div className={styles.containerUserImg}>
@@ -31,10 +31,23 @@ export default function Page() {
                     </div>
                 </div>
                 <div className={styles.inputsAluno}>
-                    <InputsCadastro name='Nome Completo'/>
-                    <InputsCadastro name='Primeiro Nome'/>
-                    <InputsCadastro name='Email'/>
+                    <InputsCadastro name='Nome Completo' length='medium' placeholder='Ex: Maria Soares da Silva' type='text'/>
+                    <InputsCadastro name='Primeiro Nome' length='small' type='text' placeholder='Ex: Maria'/>
+                    <InputsCadastro name='Email' length='large' type='email' placeholder='Ex: example@example.com'/>
+                    <InputsCadastro name='CPF' length='medium' type='text' placeholder='Ex: 000.000.000-00'/>
+                    <InputsCadastro name='Data de Nascimento' type='date' placeholder='00/00/0000'/>
+                    <InputsCadastro name='Sexo' length='small' type='select' />
+                    <InputsCadastro name='Telefone 1' type='tel' placeholder='(00) 000000000'/>
+                    <InputsCadastro name='Telefone 2' type='tel' placeholder='(00) 000000000'/>
+                    <InputsCadastro name='Horário' type='select'/>
+                    <InputsCadastro name='Modalidade'type='select'/>
                 </div>
+            </div>
+            <hr></hr>
+            <div className={styles.localization}>
+                    <InputsCadastro name='Endereço'type='text' length='biggest' placeholder='Rua exemple, 150' />
+                    <InputsCadastro name='CEP'type='text' placeholder='12345-678'/>
+                    <InputsCadastro name='Bairro'type='text' placeholder='Example'/>
             </div>
         </main>
     )
--- src/app/components/InputsCadastro/InputsCadastro.js
+++ src/app/components/InputsCadastro/InputsCadastro.js
@@ -1,10 +1,10 @@
 import styles from './InputsCadastro.module.css'
 
-export default function InputsCadastro({name}){
+export default function InputsCadastro({name, length, placeholder, type}){
     return (
-        <div className={styles.inputConfig}>
+        <div className={`${styles.inputConfig}`}>
             <label>{name}</label>
-            <input></input>
+            <input className={`${styles.input} ${styles['input_' + length]}`} type={type} placeholder={placeholder}></input>
         </div>
     )
 }
